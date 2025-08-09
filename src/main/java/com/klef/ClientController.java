464330619e478511ee52eca08c72dc6c2e8707a6
Nishitha <nishithanishitha3434@gmail.com>
2025-08-09 10:44:48 +0530

    updated project

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -13,4 +13,8 @@ public class ClientController {
 	public String demo() {
 		return "Jenkins Demo";
 	}
+	@GetMapping("/test")
+	public String test() {
+		return "Spring Boot Project using Jenkins";
+	}
 }
